1a178d96a1c389690afeaf45720fa5038092051c
DAR-tatum <148995427+DAR-tatum@users.noreply.github.com>
2025-05-02 15:43:37 -0400

    apy approx

--- clients/sui_client.py
+++ clients/sui_client.py
@@ -1,6 +1,6 @@
 from PythonCore.node_utils import AbstractNodeClient
 import requests
-import json
+import pandas as pd
 
 class SuiClient(AbstractNodeClient):
     def __init__(self, rpc_url):
@@ -8,12 +8,7 @@ class SuiClient(AbstractNodeClient):
         self.rpc_provider = rpc_url
 
     def _make_rpc_request(self, method, params=None):
-        payload = {
-            "jsonrpc": "2.0",
-            "id": 1,
-            "method": method,
-            "params": params if params else []
-        }
+        payload = {"jsonrpc": "2.0", "id": 1, "method": method, "params": params or []}
         try:
             response = requests.post(self.rpc_provider, json=payload)
             response.raise_for_status()
@@ -26,24 +21,9 @@ class SuiClient(AbstractNodeClient):
             print(f"Request Error: {e}")
             return None
 
-    def get_staking_rewards_for_epoch(self, validators, epoch):
-        epoch_rewards = 0
-
-        for address in validators:
-            response = self._make_rpc_request('suix_getStakes', [address])
-
-            if response and isinstance(response, list):
-                stakes = response[0].get('stakes', [])
-
-                for stake in stakes:
-                    if int(stake.get('stakeActiveEpoch')) == epoch:
-                        epoch_rewards += int(stake.get('estimatedReward', 0))
-
-        return epoch_rewards
-
     def get_settlement_time(self, epoch):
         result = self._make_rpc_request('suix_getLatestSuiSystemState')
-        return result.get('epochStartTimestampMs') ## TODO is this correct?
+        return result.get('epochStartTimestampMs')
 
     def get_current_epoch(self):
         result = self._make_rpc_request('suix_getLatestSuiSystemState')
@@ -51,13 +31,42 @@ class SuiClient(AbstractNodeClient):
 
     def get_total_stake(self, epoch):
         result = self._make_rpc_request('suix_getLatestSuiSystemState')
-        return int(result.get('totalStake'))
+        return int(result.get('totalStake')) / 1_000_000_000
 
-    def get_active_validators(self, current_epoch):
+    def get_active_validators(self):
         result = self._make_rpc_request('suix_getLatestSuiSystemState')
+
         active_validators = result.get('activeValidators')
-        active_validators_list = [validator.get('suiAddress') for validator in active_validators]
-        return json.dumps(active_validators_list)
+        current_epoch = int(result.get('epoch'))
+
+        rows = []
+        for validator_data in active_validators:
+            row = {
+                "validator": validator_data["suiAddress"],
+                "stake": float(validator_data["stakingPoolSuiBalance"]),
+                "epoch": current_epoch,
+            }
+            rows.append(row)
+
+        return pd.DataFrame(rows)
+
+    def get_validator_apys(self):
+        result = self._make_rpc_request('suix_getValidatorsApy')
+
+        validators_apy_data  = result.get('apys')
+        current_epoch = int(result.get('epoch'))
+
+        rows = []
+        for validator_data in validators_apy_data:
+            row = {
+                "validator": validator_data["address"],
+                "apy": float(validator_data["apy"]),
+                "epoch": current_epoch
+            }
+            rows.append(row)
+
+        return pd.DataFrame(rows)
+
 
     def get_staking_rewards(self, epoch):
         pass
--- helpers.py
+++ helpers.py
@@ -1,34 +1,8 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-# def clean(data):
-#     return data.apply(lambda row: pd.Series({
-#             'blockchain': 'SUI',
-#             'darAssetID': '???',  # TODO
-#             'darAssetTicker': 'SUI',
-#             'sedol': '???',       # TODO
-#             'periodType': 'daily',
-#             'rewardPeriodStartTime': (row['day_settled'] - timedelta(days=1)).strftime('%Y-%m-%d 00:00:00'),
-#             'rewardPeriodEndTime': row['day_settled'].strftime('%Y-%m-%d 00:00:00'),
-#             'totalRewardQuantity': float(f"{float(row['epoch_rewards']):.6f}"),
-#             'stakedQuantity': int(row['total_staked_near']),
-#             'reserved1': None,
-#             'reserved2': None,
-#             'reserved3': None,
-#             'reserved4': None,
-#             'reserved5': None,
-#             'reserved6': None,
-#             'reserved7': None,
-#             'reserved8': None,
-#             'reserved9': None,
-#             'reserved10': None
-#         }),
-#         axis=1
-#     )
-
-
 def clean(data):
-    return data.apply(lambda row: format_row(row['date'], row['total_staked'] / 1_000_000_000, row['validator_rewards'] / 1_000_000_000), axis=1)
+    return data.apply(lambda row: format_row(row['date'], row['total_staked'], row['validator_rewards']), axis=1)
 
 def format_row(date, current_staked, current_rewards):
     return pd.Series({
@@ -52,3 +26,25 @@ def format_row(date, current_staked, current_rewards):
         'reserved9': None,
         'reserved10': None
     })
+
+
+def calc_staking_rewards_for_epoch(validators, apys):
+    if len(validators) != len(apys):
+        raise ValueError(f"Length mismatch: validators={len(validators)}, apys={len(apys)}")
+
+    merged_df = pd.merge(
+        apys,
+        validators,
+        on=['validator', 'epoch'],
+        # how='inner'
+    )
+
+    merged_df['apy'] = merged_df['apy'].astype(float)
+    merged_df['stake'] = merged_df['stake'].astype(str).astype(float)  # Convert to string first to handle Decimal objects
+
+    merged_df['daily_rate'] = merged_df['apy'] / 365
+    merged_df['estimated_reward'] = merged_df['stake'] * merged_df['daily_rate']
+    merged_df['stake_sui'] = merged_df['stake'] / 1_000_000_000
+    merged_df['estimated_reward_sui'] = merged_df['estimated_reward'] / 1_000_000_000
+
+    return merged_df['estimated_reward_sui'].sum()
